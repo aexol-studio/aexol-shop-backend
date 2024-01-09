193cee2704f81cfcd27a6bfcc2e31493447fba51
AleksanderBondar <darkduble12@gmail.com>
2024-01-09 08:22:12 +0100

    :rocket:

--- src/config/api_options/index.ts
+++ src/config/api_options/index.ts
@@ -4,19 +4,16 @@ export const apiOptions: VendureConfig["apiOptions"] = {
   port: 3000,
   adminApiPath: "admin-api",
   shopApiPath: "shop-api",
-  // The following options are useful in development mode,
-  // but are best turned off for production for security
-  // reasons.
-  //   ...(IS_DEV
-  //     ? {
-  //         adminApiPlayground: {
-  //           settings: { "request.credentials": "include" },
-  //         },
-  //         adminApiDebug: true,
-  //         shopApiPlayground: {
-  //           settings: { "request.credentials": "include" },
-  //         },
-  //         shopApiDebug: true,
-  //       }
-  //     : {}),
+  ...(process.env.APP_ENV === "dev"
+    ? {
+        adminApiPlayground: {
+          settings: { "request.credentials": "include" },
+        },
+        adminApiDebug: true,
+        shopApiPlayground: {
+          settings: { "request.credentials": "include" },
+        },
+        shopApiDebug: true,
+      }
+    : {}),
 };
--- src/vendure-config.ts
+++ src/vendure-config.ts
@@ -1,4 +1,11 @@
-import { VendureConfig, DefaultLogger, LogLevel } from "@vendure/core";
+import {
+  VendureConfig,
+  DefaultLogger,
+  LogLevel,
+  LanguageCode,
+  SessionService,
+  InternalServerError,
+} from "@vendure/core";
 import {
   apiOptions,
   authOptions,
@@ -7,6 +14,66 @@ import {
   paymentOptions,
   plugins,
 } from "./config";
+
+import {
+  ActiveOrderStrategy,
+  Injector,
+  Order,
+  OrderService,
+  RequestContext,
+  TransactionalConnection,
+} from "@vendure/core";
+
+class OrderStrategy implements ActiveOrderStrategy {
+  readonly name = "active-order-strategy";
+
+  private connection: TransactionalConnection;
+  private orderService: OrderService;
+  private sessionService: SessionService;
+
+  init(injector: Injector) {
+    this.connection = injector.get(TransactionalConnection);
+    this.orderService = injector.get(OrderService);
+    this.sessionService = injector.get(SessionService);
+  }
+
+  createActiveOrder(ctx: RequestContext) {
+    return this.orderService.create(ctx, ctx.activeUserId);
+  }
+
+  async determineActiveOrder(ctx: RequestContext) {
+    if (!ctx.session) {
+      throw new InternalServerError("error.no-active-session");
+    }
+    let order = ctx.session.activeOrderId
+      ? await this.connection
+          .getRepository(ctx, Order)
+          .createQueryBuilder("order")
+          .leftJoin("order.channels", "channel")
+          .where("order.id = :orderId", { orderId: ctx.session.activeOrderId })
+          .andWhere("channel.id = :channelId", { channelId: ctx.channelId })
+          .getOne()
+      : undefined;
+    if (order && order.active === false) {
+      await this.sessionService.unsetActiveOrder(ctx, ctx.session);
+      order = undefined;
+    }
+    if (!order) {
+      if (ctx.activeUserId) {
+        order = await this.orderService.getActiveOrderForUser(
+          ctx,
+          ctx.activeUserId
+        );
+      }
+    }
+
+    // HERE is the change than core have (we recalculate price adjustments, because it gives better results)
+    if (!order) return undefined;
+    const fullOrder = await this.orderService.findOne(ctx, order?.id);
+    if (!fullOrder) return undefined;
+    return this.orderService.applyPriceAdjustments(ctx, fullOrder);
+  }
+}
 
 export const config: VendureConfig = {
   logger: new DefaultLogger({ level: LogLevel.Debug }),
@@ -17,4 +84,7 @@ export const config: VendureConfig = {
   dbConnectionOptions,
   customFields: {},
   plugins,
+  orderOptions: {
+    activeOrderStrategy: new OrderStrategy(),
+  },
 };
