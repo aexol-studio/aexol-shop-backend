27707fe9bc281cfaeedd8495428ce5979f45e7e1
Kamil Matysiewicz <kamil.matysiewicz@pm.me>
2024-10-10 17:57:42 +0200

    feat: use default config reader for S3

--- src/config/plugins/assets.ts
+++ src/config/plugins/assets.ts
@@ -4,30 +4,24 @@ import {
 } from "@vendure/asset-server-plugin";
 import { DefaultAssetNamingStrategy } from "@vendure/core";
 import path from "path";
-import { getEnvs } from "../../getEnvs";
+import { fromNodeProviderChain } from "@aws-sdk/credential-providers";
+import { getEnvs } from "src/getEnvs";
 
-const { S3_ACCESS_KEY_ID, S3_SECRET_ACCESS_KEY } =
-  getEnvs();
-
-const { S3_ENDPOINT, S3_FORCE_PATH_STYLE } = process.env;
+const region = process.env.AWS_REGION || "eu-central-1";
+const credentials = fromNodeProviderChain({
+  ...(process.env.AWS_PROFILE && { profile: process.env.AWS_PROFILE }),
+  clientConfig: { region },
+});
+const nativeS3Configuration = { region, credentials };
+const { S3_BUCKET: bucket } = getEnvs();
 
 export const AssetsPlugin = AssetServerPlugin.init({
   route: "assets",
   assetUploadDir: path.join(__dirname, "assets"),
   namingStrategy: new DefaultAssetNamingStrategy(),
   storageStrategyFactory: configureS3AssetStorage({
-    bucket: "vendure-dev",
-    credentials: {
-      accessKeyId: S3_ACCESS_KEY_ID,
-      secretAccessKey: S3_SECRET_ACCESS_KEY,
-    },
-    nativeS3Configuration: {
-      ...(S3_ENDPOINT && { endpoint: S3_ENDPOINT }),
-      ...(S3_FORCE_PATH_STYLE === "1" && { forcePathStyle: true }),
-      signatureVersion: "v4",
-      // The `region` is required by the AWS SDK even when using MinIO,
-      // so we just use a dummy value here.
-      region: "eu-west-1",
-    },
+    bucket,
+    credentials,
+    nativeS3Configuration,
   }),
 });
